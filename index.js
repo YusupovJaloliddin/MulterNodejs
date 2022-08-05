0e7ded3053aca430c84384badb41164616858a06
YusupovJaloliddin <yusupovjaloliddin4633@gmail.com>
2022-08-06 00:14:50 +0500

    i finished multer upload img

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
 
+const path = require("path")
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination:(res,file,cb)=>{
+        cb(null, "./images")
+    },
+    filename: (req,file,cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+    
+})
+
+const upload = multer({storage:storage})
+
 app.set("view engine" , "ejs");
 
 app.get("/upload",(req,res) => {
     res.render("upload");
 })
 
-app.post("/upload" , (req,res) =>{
+app.post("/upload" , upload.single('image') ,(req,res) =>{
     res.send("Image upload");
 })
 
